8871cc52514c52a028aeb5d02d2d29ef4c000f39
Abhishek Roy <61179295+Artorias17@users.noreply.github.com>
2020-05-02 02:37:43 +0600

    Create Vasya the Hipster.cpp

---
+++ Codeforces/Vasya the Hipster.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <math.h>
+int main() {
+    int a, b;
+    std::cin>>a>>b;
+    std::cout<< std::min(a,b)<< " " << (std::max(a, b)-std::min(a,b))/2 << std::endl;
+    return 0;
+}
